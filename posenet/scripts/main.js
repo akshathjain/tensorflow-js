a4e1f82c588e3e0402e2c190c67312f9fb0a9d12
Akshath Jain <akshath.r.jain@gmail.com>
2019-10-26 00:36:05 -0400

    implemented keypoint rendering

--- posenet/scripts/main.js
+++ posenet/scripts/main.js
@@ -1,34 +1,72 @@
 /*
 Name: Akshath Jain
 Date: 10/25/19
-Purpose: driver class for estimating pose
+Purpose: This file:
+            1) loads webcam video
+            2) runs each frame of the video through posenet
+            3) renders the result of posenet onto the resulting image
 */
 
+var videoWidth = 640;
+var videoHeight = 480;
+
+var color = "#00FF00";
+
+//hyper parameters to tune the network
+var confidenceLevel = 0.5;
+
+async function main(){
+    var video = await loadVideo();
+    var ctx = await loadCanvasContext();
+    var net = await loadPosenet();
+
+    video.addEventListener('play', () => renderVideo(video, net, ctx));
+    renderVideo(video, net, ctx);
+}
+main();
+
+async function renderVideo(video, net, ctx){
+    const pose = await net.estimateSinglePose(video, {
+        flipHorizontal: false,
+    });
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    ctx.drawImage(video, 0, 0);
+
+    drawLines(ctx, pose.keypoints);
+    drawKeypoints(ctx, pose.keypoints);
+    window.requestAnimationFrame(() => renderVideo(video, net, ctx));
+}
+
 async function loadVideo(){
-    console.log('here');
     var viewFrame = document.getElementById('videoViewFrame');
 
     if(navigator.mediaDevices.getUserMedia){
         var stream = await navigator.mediaDevices
                         .getUserMedia({
                             video: true,
-                            width: 600,
-                            height: 600,
+                            width: videoWidth,
+                            height: videoHeight,
                         }).catch(function(err){
                             console.log('Unable to get video: ' + err);
                         });
 
         viewFrame.srcObject = stream;
+        viewFrame.width = videoWidth;
+        viewFrame.height = videoHeight;
         return viewFrame;
     }
 }
 
 async function loadCanvasContext(){
     canvas = document.getElementById('output');
-    canvas.width = 600;
-    canvas.height = 600;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+
+    // mirror the canvas
     ctx = canvas.getContext('2d');
-    ctx.translate(600, 0);
+    ctx.translate(videoWidth, 0);
     ctx.scale(-1,1);
 
     return ctx;
@@ -42,23 +80,27 @@ async function loadPosenet(){
     return convNet;
 }
 
-async function main(){
-    video = await loadVideo();
-    ctx = await loadCanvasContext();
-    net = await loadPosenet();
-
-    video.addEventListener('play', function(){
-        async function renderVideo(){
-            const pose = await net.estimateSinglePose(video, {
-                flipHorizontal: true
-            });
-            console.log(pose);
-
-            ctx.drawImage(video, 0, 0);
-            window.requestAnimationFrame(renderVideo);
+function drawKeypoints(ctx, keypoints){
+    keypoints.forEach((point) => {
+        if(point.score > confidenceLevel){
+            ctx.beginPath();
+            ctx.arc(point.position.x, point.position.y, 3, 0, 2 * Math.PI);
+            ctx.fillStyle = color;
+            ctx.fill();
         }
-        renderVideo();
     });
 }
 
-main();
+function drawLines(ctx, keypoints){
+    //use the posenet helper method to get adjacent points (which makes
+    //our life easier)
+    var adjacentPoints = posenet.getAdjacentKeyPoints(keypoints, confidenceLevel);
+
+    adjacentPoints.forEach((point) => {
+        ctx.beginPath();
+        ctx.moveTo(point[0].position.x, point[0].position.y);
+        ctx.lineTo(point[1].position.x, point[1].position.y);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    });
+}
--- posenet/scripts/video.js
+++
@@ -1,7 +0,0 @@
-/*
-Name: Akshath Jain
-Date: 10/25/19
-Purpose: This script:
-            1) gets webcam video streamd and displays it on the webpage
-            2) draws keypoints on the video
-*/
